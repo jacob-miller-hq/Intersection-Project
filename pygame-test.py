bdd83c0f430717db657cdafbcbbba71f3697520f
jacob-miller-hq <jacob.miller.hq@gmail.com>
2020-01-29 15:08:38 -0600

    starting with pygame

---
+++ pygame-test.py
@@ -0,0 +1,30 @@
+"""
+Pygame tutorial from: https://www.pygame.org/docs/tut/PygameIntro.html
+
+More learning here: https://www.pygame.org/docs/
+"""
+import sys, pygame
+pygame.init()
+
+size = width, height = 800, 600
+speed = [1, 1.5]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
